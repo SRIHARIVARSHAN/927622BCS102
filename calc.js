12acb4719e15c7c5a0c5bd8063efbe4876152752
SRI HARIVARSHAN <sriharivarshanr@gmail.com>
2025-05-24 11:54:01 +0530

    FILE updated

--- calc.js
+++ calc.js
@@ -4,47 +4,65 @@ const axios = require('axios');
 const app = express();
 const PORT = 9876;
 
-const URL = 'http://20.244.56.144/test/numbers/';
-const VALID_IDS = ['p', 'f', 'e', 'r'];
+const URL = 'http://20.244.56.144/evaluation-service/';
+const VALID_IDS = {
+    p: 'primes',
+    f: 'fibo',
+    e: 'even',
+    r: 'rand'
+};
+
 const WINSIZE = 10;
 let numberWindow = [];
 
 async function getnum(id) {
-  try {
-    const response = await axios.get(`${URL}${id}`, { timeout: 500 });
-    return response.data.numbers || [];
-  } catch (error) {
-    return [];
-  }
+    try {
+        const response = await axios.get(`${URL}${VALID_IDS[id]}`, {
+            timeout: 500,
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ4MDY0MTc5LCJpYXQiOjE3NDgwNjM4NzksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjA4N2U1Mzc2LWI3NzctNDAxZS1hN2NhLTY3ZmUwOGE1NWM1ZiIsInN1YiI6InNyaWhhcml2YXJzaGFuckBnbWFpbC5jb20ifSwiZW1haWwiOiJzcmloYXJpdmFyc2hhbnJAZ21haWwuY29tIiwibmFtZSI6InNyaSBoYXJpdmFyc2hhbiByIiwicm9sbE5vIjoiOTI3NjIyYmNzMTAyIiwiYWNjZXNzQ29kZSI6IndoZVFVeSIsImNsaWVudElEIjoiMDg3ZTUzNzYtYjc3Ny00MDFlLWE3Y2EtNjdmZTA4YTU1YzVmIiwiY2xpZW50U2VjcmV0IjoiQWhtdnRNUktockRRdmZjVCJ9.XFJNJq8UaIAuwlkDRCmnCx_s045XGysSOjazq9EvME4'
+            }
+        });
+        console.log("API Response:", response.data);
+
+        return response.data.numbers || [];
+    } catch (error) {
+        console.log("Error fetching:", error.message);
+        return [];
+    }
 }
 
+
+
 function calcAvg(arr) {
-  if (arr.length === 0) return 0;
-  const sum = arr.reduce((acc, num) => acc + num, 0);
-  return parseFloat((sum / arr.length).toFixed(2));
+    if (arr.length === 0) return 0;
+    const sum = arr.reduce((acc, num) => acc + num, 0);
+    return parseFloat((sum / arr.length).toFixed(2));
 }
 
 app.get('/numbers/:id', async (req, res) => {
-  const id = req.params.id;
-  if (!VALID_IDS.includes(id)) {
-    return res.status(400).json({ error: 'Invalid ID. Use p, f, e, or r.' });
-  }
-  const windowPrevState = [...numberWindow];
-  const newNumbers = await getnum(id);
-  const uniqueNewNumbers = newNumbers.filter(num => !numberWindow.includes(num));
-  numberWindow = [...numberWindow, ...uniqueNewNumbers];
-  if (numberWindow.length > WINSIZE) {
-    numberWindow = numberWindow.slice(-WINSIZE); 
-  }
-  const avg = calcAvg(numberWindow);
-  res.json({
-    windowPrevState,
-    windowCurrState: numberWindow,
-    numbers: newNumbers,
-    avg
-  });
+    const id = req.params.id;
+    if (!VALID_IDS[id]) {
+        return res.status(400).json({ error: 'Invalid ID. Use p, f, e, or r.' });
+    }
+
+    const windowPrevState = [...numberWindow];
+    const newNumbers = await getnum(id);
+    const uniqueNewNumbers = newNumbers.filter(num => !numberWindow.includes(num));
+    numberWindow = [...numberWindow, ...uniqueNewNumbers];
+    if (numberWindow.length > WINSIZE) {
+        numberWindow = numberWindow.slice(-WINSIZE);
+    }
+    const avg = calcAvg(numberWindow);
+
+    res.json({
+        windowPrevState,
+        windowCurrState: numberWindow,
+        numbers: newNumbers,
+        avg
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
